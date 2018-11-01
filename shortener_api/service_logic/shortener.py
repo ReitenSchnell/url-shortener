c06d7a18fe652ed4a3c1cfe9103af2fe45c545f9
alina gryaznova <alina.gryaznova@vaultit.org>
2018-11-01 10:43:53 +0200

    adds some logic to avoid cache collisions

--- shortener_api/service_logic/shortener.py
+++ shortener_api/service_logic/shortener.py
@@ -13,6 +13,10 @@ class Shortener(object):
         self._logger.debug("starting to create shortened version of {}".format(original_url))
         hasher = UrlHasher(self._logger)
         hashed_code = hasher.get_hashed_url(original_url)
+        # we want to avoid collisions and check if the code was previously stored
+        while self._repository.read_value(hashed_code):
+            hashed_code = hasher.get_hashed_url(original_url)
+
         full_shortened_url = self._config[ENV_BASE_URL] + hashed_code
         self._logger.debug("shortened url is {}".format(full_shortened_url))
         self._repository.save_value(hashed_code, original_url)
--- tests/service_logic/shortener_test.py
+++ tests/service_logic/shortener_test.py
@@ -7,12 +7,14 @@ TEST_CONFIG = {"ENV_BASE_URL": SERVICE_URL}
 HASHED_CODE = "FooBar123"
 ORIGINAL_URL = "some url"
 URL_HASHER_PATH = "shortener_api.service_logic.url_hasher.UrlHasher.get_hashed_url"
-REPOSITORY_PATH = "shortener_api.service_logic.repository.RedisRepository.save_value"
+REPOSITORY_WRITE_PATH = "shortener_api.service_logic.repository.RedisRepository.save_value"
+REPOSITORY_READ_PATH = "shortener_api.service_logic.repository.RedisRepository.read_value"
 
 
 def test_returns_service_url_extended_with_code(mocker):
     hasher_mock = mocker.patch(URL_HASHER_PATH, return_value=HASHED_CODE)
-    mocker.patch(REPOSITORY_PATH)
+    mocker.patch(REPOSITORY_WRITE_PATH)
+    mocker.patch(REPOSITORY_READ_PATH, return_value=None)
     test_log = get_null_log()
     repository = RedisRepository(test_log, TEST_CONFIG)
     shortener = Shortener(test_log, TEST_CONFIG, repository)
@@ -25,7 +27,8 @@ def test_returns_service_url_extended_with_code(mocker):
 
 def test_saves_original_and_shortened_url_to_repository(mocker):
     mocker.patch(URL_HASHER_PATH, return_value=HASHED_CODE)
-    repository_mock = mocker.patch(REPOSITORY_PATH)
+    mocker.patch(REPOSITORY_READ_PATH, return_value=None)
+    repository_mock = mocker.patch(REPOSITORY_WRITE_PATH)
     test_log = get_null_log()
     repository = RedisRepository(test_log, TEST_CONFIG)
     shortener = Shortener(test_log, TEST_CONFIG, repository)
@@ -33,3 +36,20 @@ def test_saves_original_and_shortened_url_to_repository(mocker):
     shortener.create_shortened_url(ORIGINAL_URL)
 
     repository_mock.assert_called_once_with(HASHED_CODE, ORIGINAL_URL)
+
+
+def test_regenerates_and_stores_hash_code_if_it_already_taken(mocker):
+    hasher_mock = mocker.patch(URL_HASHER_PATH)
+    hasher_mock.side_effect = ["hash1", "hash2", "hash3"]
+    repository_read_mock = mocker.patch(REPOSITORY_READ_PATH)
+    repository_read_mock.side_effect = ["url1", "url2", None]
+    repository_mock = mocker.patch(REPOSITORY_WRITE_PATH)
+    test_log = get_null_log()
+    repository = RedisRepository(test_log, TEST_CONFIG)
+    shortener = Shortener(test_log, TEST_CONFIG, repository)
+
+    shortener.create_shortened_url(ORIGINAL_URL)
+
+    assert 3 == hasher_mock.call_count
+    assert 3 == repository_read_mock.call_count
+    repository_mock.assert_called_once_with("hash3", ORIGINAL_URL)
